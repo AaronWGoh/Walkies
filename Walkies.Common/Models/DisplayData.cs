654aa7fb0f75a1df1973b64b9575dba8264683bb
AaronGoh <aaronwgoh5@gmail.com>
2018-09-09 06:26:38 -0600

    Map no longer using static embedded. Runs static API

--- Walkies.Common/Models/DisplayData.cs
+++ Walkies.Common/Models/DisplayData.cs
@@ -8,5 +8,7 @@ namespace Walkies.Common.Models
     {
         public IEnumerable<Shelter> Shelters { get; set; }
         public IEnumerable<Dog> Dogs { get; set; }
+        public String City { get; set; }
+        public String StateCode { get; set; }
     }
 }
--- Walkies.Web/Controllers/MapController.cs
+++ Walkies.Web/Controllers/MapController.cs
@@ -3,19 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Walkies.Common.Models;
+using Walkies.DatabaseOperations;
 
 namespace Walkies.Web.Controllers
 {
     public class MapController : Controller
     {
-        public IActionResult Index()
+        private IConfiguration _config;
+        private ShelterRepository _shelterRepo;
+        private DogRepository _dogRepo;
+
+        public MapController(IConfiguration config, ShelterRepository shelterRepo, DogRepository dogRepo)
         {
-            return View();
+            _config = config;
+            _shelterRepo = shelterRepo;
+            _dogRepo = dogRepo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Shelter> shelters = await _shelterRepo.GetAll();
+            IEnumerable<Dog> dogs = await _dogRepo.GetAll();
+
+            DisplayData displayData = new DisplayData
+            {
+                Shelters = shelters,
+                Dogs = dogs,
+                City = "Salt Lake City",
+                StateCode = "UT"
+            };
+
+            return View(displayData); 
         }
 
-        public IActionResult DogWalker()
+        public async Task<IActionResult> DogWalker()
         {
-            return View();
+            IEnumerable<Shelter> shelters = await _shelterRepo.GetAll();
+            IEnumerable<Dog> dogs = await _dogRepo.GetAll();
+
+            DisplayData displayData = new DisplayData
+            {
+                Shelters = shelters,
+                Dogs = dogs,
+                City = "Salt Lake City",
+                StateCode = "UT"
+            };
+
+            return View(displayData);
         }
     }
 }
